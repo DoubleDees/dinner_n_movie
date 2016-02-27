0e245a27963d1c9101d14dbe6f3fab9851fad2d4
Daniel Buompane <danielbuompane@gmail.com>
2016-02-27 14:44:18 -0500

    make index action list all playlists found for food genre

--- app/controllers/gracenotes_controller.rb
+++ app/controllers/gracenotes_controller.rb
@@ -1,10 +1,12 @@
 class GracenotesController < ApplicationController
   def index
+    r = Gracenote.new("japanese")
+    render json: r.all_playlists
 
   end
 
   def show
-    r = Gracenote.new("mongolian")
-    render json: r.playlists
+    r = Gracenote.new("sushi")
+    render json: r.random_playlist
   end
 end
--- app/models/gracenote.rb
+++ app/models/gracenote.rb
@@ -5,9 +5,14 @@ class Gracenote
     @response = HTTParty.get("https://api.spotify.com/v1/search?q=#{genre}&type=playlist")
   end
 
-  def playlists
+  def random_playlist
   playlist = @response["playlists"]["items"]
   playlist.map{|l| l["external_urls"]}.sample
+  end
+
+  def all_playlists
+    all_playlists = @response["playlists"]["items"]
+    all_playlists.each {|l| l["external_urls"]}
   end
 
 
