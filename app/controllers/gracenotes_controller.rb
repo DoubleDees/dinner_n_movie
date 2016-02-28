d143b9c29719fb0df5e3f3bcb227d0219d777559
Daniel Buompane <danielbuompane@gmail.com>
2016-02-27 19:48:22 -0500

    add params to controller

--- app/controllers/gracenotes_controller.rb
+++ app/controllers/gracenotes_controller.rb
@@ -1,12 +1,12 @@
 class GracenotesController < ApplicationController
   def index
-    r = Gracenote.new("japanese")
+    r = Gracenote.new(params[:genre])
     render json: r.all_playlists
 
   end
 
   def show
-    r = Gracenote.new("sushi")
+    r = Gracenote.new(params[:genre])
     render json: r.random_playlist
   end
 end
